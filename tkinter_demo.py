8535d63976e5b00c5bcac46a41ccb98c1974948f
cacakfi20 <95623700+cacakfi20@users.noreply.github.com>
2022-02-24 15:38:02 +0100

    aplikace demo

---
+++ tkinter_demo.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+
+window = tk.Tk()
+window.geometry("600x300")
+window.title("v.1.1.")
+
+label = tk.Label(text="Je to možný")
+label.pack()
+
+button = tk.Button()
+button.geometry
+
+window.mainloop()
+
