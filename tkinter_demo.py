ba3d343288e49f6c69494f506ba7071033d76475
cacakfi20 <95623700+cacakfi20@users.noreply.github.com>
2022-03-03 14:41:53 +0100

    funkcni counter

--- tkinter_demo.py
+++ tkinter_demo.py
@@ -1,6 +1,5 @@
 import tkinter as tk
 
-
 number = [0]
 def add(n):
     n.append(n[-1] + 1)
@@ -11,7 +10,19 @@ window = tk.Tk()
 window.geometry("600x300")
 window.title("v.1.1.")
 
-button = tk.Button(window, text="Klikni pro přičtení", command=lambda:add(number))
+button = tk.Button(
+    text = "Klikni pro přičtení",
+    width = 20,
+    height = 5,
+    fg = "violet",
+    command = lambda:add(number)
+)
 button.pack()
+
+uzivatel_entry = tk.Entry(
+    width=20,
+
+)
+uzivatel_entry.pack()
 
 window.mainloop()
